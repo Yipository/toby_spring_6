a05db502653bae1a1cc3bb147c1a9f677f565225
Yipository <goatjswn.work@gmail.com>
2024-06-29 20:31:13 +0900

    Initial Commit

---
+++ src/main/java/spring/toby_spring_6/TobySpring6Application.java
@@ -0,0 +1,13 @@
+package spring.toby_spring_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpring6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobySpring6Application.class, args);
+	}
+
+}
---
+++ src/test/java/spring/toby_spring_6/TobySpring6ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.toby_spring_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpring6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
