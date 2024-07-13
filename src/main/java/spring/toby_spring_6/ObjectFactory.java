39b1f40b3472f87378c7e6b5628bbbd77ce67215
Yipository <goatjswn.work@gmail.com>
2024-07-13 13:39:58 +0900

    Spring Container and Dependency Injection : By BeanFactory

--- src/main/java/spring/toby_spring_6/ObjectFactory.java
+++ src/main/java/spring/toby_spring_6/ObjectFactory.java
@@ -1,11 +1,17 @@
 package spring.toby_spring_6;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exrateProvider());
     }
 
+    @Bean
     public ExRateProvider exrateProvider() {
-        return new SimpleExRateProvider();
+        return new WebApiExRateProvider();
     }
 }
